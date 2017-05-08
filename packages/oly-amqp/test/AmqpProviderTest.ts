242fcf201f0a3025d8329746cc785e5b3052b93f
nfo <ni.foures@gmail.com>
2017-05-08 21:34:23 +0200

    Fix coverage and broken tests

--- packages/oly-amqp/test/AmqpProviderTest.ts
+++ packages/oly-amqp/test/AmqpProviderTest.ts
@@ -4,38 +4,38 @@ import { task } from "../src";
 import { AmqpProvider } from "../src/providers/AmqpProvider";
 import { WorkerProvider } from "../src/providers/WorkerProvider";
 
-describe("AmqpProvider", () => {
+class Task {
+  @inject amqp: AmqpProvider;
 
-  class Task {
-    @inject amqp: AmqpProvider;
-
-    @task({
-      assert: {
-        messageTtl: 1000,
-      },
-      name: "TEST",
-      priority: 10,
-      retry: 4,
-    }) index(payload: any, message: Message) {
-      const data = this.amqp.extract(message);
-      if (payload.WUT === 2 && data.attempts < 2) {
-        throw new Error("LOL");
-      }
-      stack.push(payload);
-      if (stack.length === 4) {
-        k2.emit("END");
-      }
+  @task({
+    assert: {
+      messageTtl: 1000,
+    },
+    name: "TEST",
+    priority: 10,
+    retry: 4,
+  })
+  index(payload: any, message: Message) {
+    const data = this.amqp.extract(message);
+    if (payload.WUT === 2 && data.attempts < 2) {
+      throw new Error("LOL");
+    }
+    stack.push(payload);
+    if (stack.length === 4) {
+      k2.emit("END");
     }
   }
+}
 
-  const stack: any[] = [];
-  const k1 = new Kernel({
-    OLY_LOGGER_LEVEL: "ERROR",
-    OLY_QUEUE_RETRY_DELAY: 10,
-  }).with(Task, WorkerProvider);
+const stack: any[] = [];
+const k1 = new Kernel({
+  OLY_LOGGER_LEVEL: "ERROR",
+  OLY_QUEUE_RETRY_DELAY: 10,
+}).with(Task, WorkerProvider);
 
-  const k2 = new Kernel({OLY_LOGGER_LEVEL: "ERROR"})
-    .with(AmqpProvider);
+const k2 = new Kernel({OLY_LOGGER_LEVEL: "ERROR"}).with(AmqpProvider);
+
+describe("AmqpProvider", () => {
 
   beforeAll(async () => {
     await k1.start();
