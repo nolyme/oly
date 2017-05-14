1017ca065f0efe9d2b05e36231b755246c2047cb
nfo <ni.foures@gmail.com>
2017-05-14 21:19:41 +0200

    Add more tests

--- packages/oly-cron/test/CronProviderTest.ts
+++ packages/oly-cron/test/CronProviderTest.ts
@@ -1,43 +1,35 @@
-import { _, inject, Kernel } from "oly-core";
-import { cron } from "../src/annotations";
+import { _, inject } from "oly-core";
+import { attachKernel } from "oly-test";
+import { cron } from "../src";
 import { CronProvider } from "../src/CronProvider";
+
+const stack: any[] = [];
+
+class App {
+
+  @cron("* * * * * *")
+  repeat() {
+    stack.push("OK");
+    if (stack.length === 2) {
+      throw new Error("LOL");
+    }
+  }
+}
+
+class Mockery {
+  @inject cron: CronProvider;
+
+  onConfigure() {
+    (this.cron as any).jobs[0].cronTime.getTimeout = () => 50;
+  }
+}
 
 describe("CronProvider", () => {
 
-  let kernel: Kernel;
-
-  afterAll(() => kernel.stop());
+  attachKernel().with(Mockery, App);
 
   it("should tick once", async () => {
-
-    const stack: any[] = [];
-
-    class App {
-
-      @cron("* * * * * *")
-      repeat() {
-        stack.push("OK");
-        if (stack.length === 2) {
-          throw new Error("LOL");
-        }
-      }
-    }
-
-    class Mockery {
-      @inject cron: CronProvider;
-
-      onConfigure() {
-        (this.cron as any).jobs[0].cronTime.getTimeout = () => 50;
-      }
-    }
-
-    kernel = new Kernel({OLY_LOGGER_LEVEL: "ERROR"});
-    kernel.with(Mockery, App);
-
-    await kernel.start();
     await _.timeout(250);
-    await kernel.stop();
-
     expect(stack.length).toBeGreaterThanOrEqual(3);
   });
 });
