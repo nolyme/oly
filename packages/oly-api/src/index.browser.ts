c0de9b68530a21561ef5b6d6f8ebb52f1be8e824
nfo <ni.foures@gmail.com>
2017-06-27 16:21:28 +0200

    (oly-api): open decorator for browsers

--- packages/oly-api/src/index.browser.ts
+++ packages/oly-api/src/index.browser.ts
@@ -1,1 +1,16 @@
-// noop
+export * from "./constants/errors";
+export * from "./interfaces";
+export * from "./decorators/body";
+export * from "./decorators/del";
+export * from "./decorators/get";
+export * from "./decorators/param";
+export * from "./decorators/post";
+export * from "./decorators/put";
+export * from "./decorators/query";
+export * from "./exceptions/BadRequestException";
+export * from "./exceptions/NotFoundException";
+export * from "./exceptions/UnauthorizedException";
+export * from "./exceptions/ForbiddenException";
+export * from "./exceptions/MethodNotAllowedException";
+export * from "./exceptions/UnauthorizedException";
+export * from "./exceptions/ConflictException";
